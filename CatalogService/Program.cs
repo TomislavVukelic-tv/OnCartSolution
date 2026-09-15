ee816269d5f894e7b8ac10b66162fac002e88e2c
Tomislav Vukelic <tomicavukelic@gmail.com>
2026-09-16 00:15:30 +0200

    Added initial Program startup configuration for Catalog Service

--- CatalogService/Program.cs
+++ CatalogService/Program.cs
@@ -1,20 +1,141 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi;
 using Rhetos;
 
-namespace CatalogService
+namespace CatalogService;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        ConfigureServices(builder.Services, builder.Configuration);
+
+        var app = builder.Build();
+
+        ConfigurePipeline(app);
+
+        app.Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Microsoft.Extensions.Hosting.Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureServices((context, services) =>
+                {
+                    ConfigureServices(
+                        services,
+                        context.Configuration);
+                });
+            });
+    }
+
+    private static void ConfigureServices(
+        IServiceCollection services,
+        IConfiguration configuration)
+    {
+        
+        services
+            .AddRhetosHost((serviceProvider, rhetosHostBuilder) =>
+            {
+                rhetosHostBuilder
+                    .ConfigureRhetosAppDefaults()
+                    .UseBuilderLogProviderFromHost(serviceProvider)
+                    .ConfigureConfiguration(cfg =>
+                        cfg
+                            .MapNetCoreConfiguration(configuration)
+                            .AddJsonFile(
+                                "rhetos-app.local.settings.json",
+                                optional: true));
+            })
+            .AddAspNetCoreIdentityUser()
+            .AddHostLogging()
+            .AddDashboard()
+            .AddRestApi(options =>
+            {
+                options.BaseRoute = "rest";
+
+                options.GroupNameMapper =
+                    (conceptInfo, controller, oldName) => "v1";
+            });
+
+        services.AddControllers();
+
+        services.AddSwaggerGen(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            options.CustomSchemaIds(type => type.ToString());
 
-            app.MapGet("/", () => "Hello World!");
+            options.SwaggerDoc(
+                "v1",
+                new OpenApiInfo
+                {
+                    Title = "CatalogService",
+                    Version = "v1"
+                });
+        });
 
-            app.Run();
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                //options.Authority = configuration["Jwt:Authority"]; not needed in demo
+                options.Audience = configuration["Jwt:Audience"];
+                options.RequireHttpsMetadata = true;
+            })
+            .AddCookie(options =>
+            {
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode =
+                        StatusCodes.Status401Unauthorized;
 
+                    return Task.CompletedTask;
+                };
+            });
+
+        services.AddAuthorization();
+
+
+        var connectionString =
+            configuration.GetConnectionString("RhetosConnectionString")!;
+
+        services
+            .AddHealthChecks()
+            .AddSqlServer(
+                connectionString,
+                name: "sql-server",
+                tags: ["ready"]);
+    }
+
+    private static void ConfigurePipeline(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint(
+                    "/swagger/v1/swagger.json",
+                    "CatalogService v1");
+            });
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseRhetosRestApi();
+
+        app.MapControllers();
+
+        app.MapRhetosDashboard();
+
+        app.MapHealthChecks("/health");
     }
 }
